40f2d140c3657cf319939fd88300f87a35dc37fa
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-06-16 23:23:52 -0400

    agregar publicPath

--- my-project/mechanic.config.js
+++ my-project/mechanic.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   hooks: [],
   port: 3000,
-  basePath: "/interactive-letters/"
+  basePath: "/interactive-letters/",
+  // https://github.com/designsystemsinternational/mechanic/issues/185
+  publicPath: "/interactive-letters/",
 };
